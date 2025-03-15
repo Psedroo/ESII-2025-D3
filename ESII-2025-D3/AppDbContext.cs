5361756f8ffbadd3eb0d85545ceb0093d8a33025
simaobarbosa132005 <simaobarbosa132005@gmail.com>
2025-03-15 15:36:49 +0000

    Insercao de user e primeiros exemplos

---
+++ ESII-2025-D3/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Artist> Artists { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Configurações adicionais do modelo podem ser adicionadas aqui
+    }
+}
