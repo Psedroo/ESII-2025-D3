ca5eac72dd2147b4e191e9e8cb9a4120546d7653
SimaoBarbosaOrg <simaobarbosa132005@gmail.com>
2025-05-16 14:18:17 +0100

    AtividadeParticipanteDB

---
+++ es2real/Models/AtividadeParticipante.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using ES2Real.Models;
+
+public class AtividadeParticipante
+{
+    [ForeignKey("Atividade")]
+    public int IdAtividade { get; set; }
+
+    [ForeignKey("Participante")]
+    public int IdParticipante { get; set; }
+
+    public Atividade Atividade { get; set; } = null!;
+    public Participante Participante { get; set; } = null!;
+}
